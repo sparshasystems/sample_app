fe41213fde09e3eada6ce7de1a4f73abc83daa66
vinod <your.email@example.com>
2013-07-08 17:06:32 +0530

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,8 +12,14 @@ describe "Static pages" do
     it "should have the title 'Home'" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Home")
+                        :text => "Ruby on Rails Tutorial Sample App")
     end
+
+it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
+
   end
 
   describe "Help page" do
@@ -23,11 +29,17 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Help')
     end
 
-    it "should have the title 'Help'" do
+    it "should have the base title" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Help")
+                        :text => "Ruby on Rails Tutorial Sample App")
     end
+
+it "should not have a custom page title" do
+  visit '/static_pages/help'
+  page.should_not have_selector('title', :text => '| Help')
+end
+
   end
 
   describe "About page" do
@@ -40,7 +52,11 @@ describe "Static pages" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App | About Us")
+                    :text => "Ruby on Rails Tutorial Sample App")
     end
+    it "should not have a custom page title" do
+  visit '/static_pages/about'
+  page.should_not have_selector('title', :text => '| About Us')
+end
   end
 end
